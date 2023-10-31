ea3c3305d2b764d0b7345d1ea613721c1c3f6327
johnunicorndoe <75097058+johnunicorndoe@users.noreply.github.com>
2023-10-31 16:43:17 +0330

    Update 05_Categorized_Sales.py

--- pages/05_Categorized_Sales.py
+++ pages/05_Categorized_Sales.py
@@ -3,8 +3,8 @@ import streamlit as st
 import plotly.express as px
 
 # Function to load Excel data
-def load_data():
-    return pd.read_excel('hadron.xlsx', header=0, nrows=7, sheet_name="Categorized Sales")
+def load_data(file_path, sheet_name):
+    return pd.read_excel(file_path, header=0, nrows=7, sheet_name=sheet_name)
 
 # Function to filter the DataFrame based on selected months and columns
 def filter_data(df, selected_months, selected_columns):
@@ -15,8 +15,8 @@ def filter_data(df, selected_months, selected_columns):
 def calculate_column_totals(df):
     return df.drop("Month", axis=1).sum()
 
-# Function to display the data and column totals
-def display_data(filtered_df, column_totals):
+# Function to display a data table and column totals
+def display_data_table(filtered_df, column_totals):
     st.markdown("---")
     col1, col2 = st.columns(2)
 
@@ -25,9 +25,16 @@ def display_data(filtered_df, column_totals):
     col2.dataframe(pd.DataFrame({"Totals": column_totals}))
 
 # Function to create and display a line chart
-def display_line_chart(filtered_data, x_column, y_columns):
-    if len(filtered_data) > 0:
-        fig = px.line(filtered_data, x=x_column, y=y_columns, markers=True, line_shape="linear")
+def display_line_chart(data, x_column, y_columns):
+    if len(data) > 0:
+        fig = px.line(data, x=x_column, y=y_columns, markers=True, line_shape="linear")
+
+        # Update the legend title
+        fig.update_layout(legend_title_text="Product Category")
+
+        # Update Y-axis title from "title" to "quantity"
+        fig.update_yaxes(title_text="Quantity")
+
         st.plotly_chart(fig)
     else:
         st.warning("No data selected for the chart. Please select at least one month and one category.")
@@ -39,9 +46,27 @@ def main():
         layout="wide"
     )
 
+    # Hide Streamlit's menu, header, and footer
+    st.markdown(
+        """
+        <style>
+            #MainMenu { visibility:hidden; }
+            header { visibility: hidden; }
+            footer { visibility: hidden; }
+        </style>
+        """, unsafe_allow_html=True
+    )
+
+    # Title
     st.title('Categorized Sales')
 
-    df = load_data()
+    # Filter
+    st.subheader("Please Filter Here:")
+
+    file_path = 'hadron.xlsx'
+    sheet_name = "Categorized Sales"
+
+    df = load_data(file_path, sheet_name)
 
     available_columns = [col for col in df.columns if col != "Month"]
     selected_columns = st.multiselect("Select Categories:", available_columns, default=available_columns)
@@ -53,8 +78,8 @@ def main():
     filtered_df = filter_data(df, selected_months, selected_columns)
     column_totals = calculate_column_totals(filtered_df)
 
-    # Display dataframes first
-    display_data(filtered_df, column_totals)
+    # Display data table and column totals
+    display_data_table(filtered_df, column_totals)
 
     # Check if "Speaker" and "Cable" are in selected_columns
     if "Speaker" in selected_columns and "Cable" in selected_columns:
@@ -68,7 +93,7 @@ def main():
         elif "Cable" in selected_columns:
             display_line_chart(filtered_df, "Month", ["Cable"])
         else:
-            # If both are deselected, show a plain chart axis
+            # If both are deselected, show an empty chart
             st.plotly_chart(px.line(pd.DataFrame(columns=["Month"]), x="Month"))
 
     # Create a list of selected columns excluding "Speaker" and "Cable"
